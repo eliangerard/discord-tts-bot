6f2218eb473dcfdcf7c1cd2ecc9391475f806a0c
Elian <eliangerardiso@gmail.com>
2023-08-01 13:28:38 -0600

    Replaced project

--- index.js
+++ index.js
@@ -1,73 +1,40 @@
-const fs = require('fs');
-const { Intents } = require("discord.js");
-const { DisTube } = require("distube");
-const Discord = require("discord.js");
-const config = require("./config.json")
+const express = require('express');
+const cors = require('cors');
+const { searchRouter } = require('./routes');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
 
-const client = new Discord.Client({
-  intents: [
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_VOICE_STATES,
-    Intents.FLAGS.GUILD_BANS,
-  ],
-  restRequestTimeout: 30000,
-  shards: "auto",
-});
-client.config = require("./config.json")
-client.commands = new Discord.Collection()
-client.aliases = new Discord.Collection()
+const app = express();
+const port = argv.port || 3000;
 
-client.distube = new DisTube(client, {
-  searchSongs: 10,
-  emitNewSongOnly: true
-})
+app.use(cors());
 
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.log("No se encontraron comandos")
-  const jsFiles = files.filter(f => f.split(".").pop() === "js")
-  if (jsFiles.length <= 0) return console.log("No se encontraron comandos")
-  jsFiles.forEach(file => {
-    const cmd = require(`./commands/${file}`)
-    console.log(`Comando ${file} cargado`)
-    client.commands.set(cmd.name, cmd)
-    if (cmd.aliases) cmd.aliases.forEach(alias => client.aliases.set(alias, cmd.name))
-  })
-})
-
-client.on('ready', function () {
-  client.user.setActivity("a los otros bots mecos", {
-    type: "LISTENING"
-  });
-  console.log(`${client.user.tag} está listo`)
-  i = 0;
+app.get('/deezer/*', async (req, res) => {
+	try {
+		const deezerApiUrl = `https://api.deezer.com${req.path.replace('/deezer', '')}`;
+		const queryParams = new URLSearchParams(req.query).toString();
+		const response = await fetch(`${deezerApiUrl}?${queryParams}`);
+		const data = await response.json();
+		res.json(data);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 });
 
-client.on("messageCreate", async (message) => {
-  //const prefix = config.prefix
-  //if(!message.content.startsWith(prefix))return
-  var messi = message.content.split(' ');
-  const command = messi[0].toLowerCase()
-  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
-  if (!cmd) return
-  if (cmd.inVoiceChannel && !message.member.voice.channel) return message.channel.send(`${client.emotes.error} | Tienes que estar en un canal de voz`)
-  try {
-    cmd.run(client, message, messi)
-  } catch (e) {
-    console.error(e)
-    message.reply(`Error: ${e}`)
-  }
-});
-
-client.distube.on("empty", channel => {
-  const embed = new Discord.MessageEmbed()
-    .setTitle(client.emotes.sad + " Soledad")
-    .setColor("#1111EE")
-    .setDescription("No hay nadie en el canal de voz, saliendo...")
-    .setTimestamp()
-    .setFooter('Memer', botUrl)
-  message.channel.send({ embeds: [embed] })
+app.get('/oauth', async (req, res) => {
+	if (req.query.code.trim().length > 0) {
+		const response = await fetch('https://connect.deezer.com/oauth/access_token.php?app_id=618944&secret=35a5f3077061e414bfac3b06ec1c31d6&output=json&code=' + req.query.code).then(res => res.text());
+		if (response != "wrong code")
+			res.json(JSON.parse(response));
+		else res.json({ error: response });
+	}
+	else
+		res.json({ error: "There is no grant code in your request" });
 })
 
-client.login(config.token);
+app.use('/search', searchRouter);
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
